8727f9ea54623e05832848358e940ce46d9ac67f
DWhittley <115023269+DWhittley@users.noreply.github.com>
2022-11-11 07:54:35 -0500

    Initial commit

---
+++ Q2/Main.cpp
@@ -0,0 +1,16 @@
+/* Create a class called Rational for performing arithmetic with fractions. Write a program to test
+your class. Use integer variables to represent the private data of the class— the numerator and the
+denominator. Provide a constructor that enables an object of this class to be initialized when it’s
+declared. The constructor should contain default values in case no initializers are provided and
+should store the fraction in reduced form.
+
+For example, the fraction 2/4 would be stored in the object as 1 in the numerator and 2 in the denominator. 
+
+Provide public member functions that perform each of the following tasks:
+a. add—Adds two Rational numbers. The result should be stored in reduced form.
+b. subtract—Subtracts two Rational numbers. Store the result in reduced form.
+c. multiply—Multiplies two Rational numbers. Store the result in reduced form.
+d. divide—Divides two Rational numbers. The result should be stored in reduced form.
+e. toRationalString—Returns a string representation of a Rational number in the form a/b, where
+a is the numerator and b is the denominator.
+f. toDouble—Returns the Rational number as a double. */
